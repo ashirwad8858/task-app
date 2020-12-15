7725590f207f96512261987b5a3501ab20b76115
unknown <ashirwadsharma12795@gmail.com>
2020-12-15 18:15:22 +0530

    adding async and await in code in place of then and catch.

--- src/index.js
+++ src/index.js
@@ -10,76 +10,116 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.post('/users',(req,res)=>{
+app.post('/users', async (req,res)=>{
 
-    const user = new User(req.body)
-
-    user.save().then(()=>{
-        res.status(201).send(user)
-    }).catch((e)=>{
-        res.status(400).send(e)
-    })
+    try{
+        const user = new User(req.body)
+        await user.save()
+        res.send(user)
+    }catch(e){
+        res.status(400).send()
+    }
+    // user.save().then(()=>{
+    //     res.status(201).send(user)
+    // }).catch((e)=>{
+    //     res.status(400).send(e)
+    // })
 
     
 })
 
 
-app.post('/task',(req,res)=>{
+app.post('/task',async (req,res)=>{
     const task = new Task(req.body)
-
-    task.save().then(()=>{
-        res.status(201).send(task)
-    }).catch((er)=>{
-        res.status(400).send(er)
-    })
-})
-
-app.get('/users',(req,res)=>{
-    User.find({}).then((user)=>{
-        res.send(user)
-    }).catch((er)=>{
-        res.status(500).send()
-    })
-})
-
-
-app.get('/users/:id',(req,res)=>{
-
-    const _id = req.params.id
-
-    User.findById(_id).then((user)=>{
-        if(!user){
-            return res.status(404).send('No User found')
-        }
-
-        res.send(user)
-    }).catch((er)=>{
-        res.status(500).send()
-    })
-})
-
-
-app.get('/task',(req,res)=>{
-    Task.find({}).then((task)=>{
+    try{
+        await task.save()
         res.send(task)
-    }).catch((er)=>{
-        res.status(500).send()
-    })
+    }catch(e){
+        res.status(400).send(e)
+    }
+
+    // task.save().then(()=>{
+    //     res.status(201).send(task)
+    // }).catch((er)=>{
+    //     res.status(400).send(er)
+    // })
+})
+
+app.get('/users',async (req,res)=>{
+    try{
+        const user = await User.find({})
+        res.send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+    
+    // User.find({}).then((user)=>{
+    //     res.send(user)
+    // }).catch((er)=>{
+    //     res.status(500).send()
+    // })
 })
 
 
-app.get('/task/:id',(req,res)=>{
-    const _id = req.params.id
+app.get('/users/:id',async (req,res)=>{
 
-    Task.findById(_id).then((task)=>{
+    const _id = req.params.id
+    try{
+        const user = await User.findById(_id)
+        res.send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+
+    // User.findById(_id).then((user)=>{
+    //     if(!user){
+    //         return res.status(404).send('No User found')
+    //     }
+
+    //     res.send(user)
+    // }).catch((er)=>{
+    //     res.status(500).send()
+    // })
+})
+
+
+app.get('/task', async (req,res)=>{
+    try{
+        const task = await Task.find({})
+        res.send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
+
+    // Task.find({}).then((task)=>{
+    //     res.send(task)
+    // }).catch((er)=>{
+    //     res.status(500).send()
+    // })
+})
+
+
+app.get('/task/:id',async (req,res)=>{
+    const _id = req.params.id
+    try{
+        const task = await Task.findById(_id)
         if(!task){
             return res.send('No task found')
         }
+        
+        res.send(task) 
+    }catch(e){
+        res.status(500).send(e)
+    }
+    // Task.findById(_id).then((task)=>{
+    //     if(!task){
+    //         return res.send('No task found')
+    //     }
 
-        res.send(task)
-    }).catch((er)=>{
-        res.status(500).send();
-    })
+    //     res.send(task)
+    // }).catch((er)=>{
+    //     res.status(500).send()
+    // })
 })
 app.listen(port,()=>{
     console.log('Server is up and running on '+port)
